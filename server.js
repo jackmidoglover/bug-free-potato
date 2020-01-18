22eae282a44cc806278a5ef95e972b0c944287c1
jackmidoglover <jackmidoglover@gmail.com>
2020-01-17 21:14:06 -0700

    static serve up issue

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/build/index.html")));
 
 
 // catch 404 and forward to error handler
@@ -24,11 +24,12 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 // error handler
