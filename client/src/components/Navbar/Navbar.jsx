1a1a2f5bbe614cc21a011d503d2fe4e15d10ab0f
jackmidoglover <jackmidoglover@gmail.com>
2020-01-29 20:26:41 -0700

    mobile nav added, a little mobile adjustment to cards

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,8 +7,14 @@ const Navbar = (props) => {
         <nav>
     <div className="nav-wrapper grey lighten-2">
     <img src="./assets/images/crystal.svg" alt="Crystal" /><a href="/" className="brand-logo purple-text text-darken-4">Jacq Glover</a>
-    
+    <a href="#" data-target="mobile-nav" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
+
       <ul id="nav-mobile" className="right hide-on-med-and-down">
+      <li><NavLink exact to="/" className="purple-text text-darken-4" activeClassName="active"><strong>About</strong></NavLink></li>
+        <li><NavLink exact to="/projects" className="purple-text text-darken-4" activeClassName="active"><strong>Projects</strong></NavLink></li>
+        <li><a href="https://www.linkedin.com/in/jackmidoglover/" className="purple-text text-darken-4"><strong>LinkedIn</strong></a></li>
+      </ul>
+      <ul id="mobile-nav" className="right sidenav">
       <li><NavLink exact to="/" className="purple-text text-darken-4" activeClassName="active"><strong>About</strong></NavLink></li>
         <li><NavLink exact to="/projects" className="purple-text text-darken-4" activeClassName="active"><strong>Projects</strong></NavLink></li>
         <li><a href="https://www.linkedin.com/in/jackmidoglover/" className="purple-text text-darken-4"><strong>LinkedIn</strong></a></li>
