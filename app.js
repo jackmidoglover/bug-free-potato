e730da459b1ad37bb4e5860b53a88b0a2d5c62b2
jackmidoglover <jackmidoglover@gmail.com>
2020-03-20 20:32:08 -0600

    replacing error handler

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ app.use(function(req, res, next) {
 app.use('/*', indexRouter);
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.json({ error: err })
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err })
+});
 
 app.listen(process.env.PORT || "8000", () => {
   console.log("✨ You're cooking with gas now girlie ✨");
