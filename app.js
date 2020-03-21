48cac9a400e8b22044f9b8b3f30b1490ce37244c
jackmidoglover <jackmidoglover@gmail.com>
2020-03-20 19:17:41 -0600

    oops

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 
 // catch 404 and forward to error handler
@@ -25,6 +25,7 @@ app.use(function(req, res, next) {
 
 
 app.use('/', indexRouter);
+
 
 
 
