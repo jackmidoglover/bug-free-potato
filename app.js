d319324578441f71399d3f52dd2e33f033016687
jackmidoglover <jackmidoglover@gmail.com>
2020-03-20 21:20:23 -0600

    fixed some package issues

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var routes = require('./routes');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -12,17 +13,17 @@ var PORT = process.env.PORT || "8000";
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-
-
-app.use(routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+app.use('/*', indexRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/projects', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
-         if (err) {
-           res.status(500).send(err)
-         }
-       });
-});
+// router.get('/projects', function(req, res, next) {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
+//          if (err) {
+//            res.status(500).send(err)
+//          }
+//        });
+// });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
-         if (err) {
-           res.status(500).send(err)
-         }
-       });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
+//          if (err) {
+//            res.status(500).send(err)
+//          }
+//        });
+// });
 
 router.get('/*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
